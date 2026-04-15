d918163483e949fa78192545fcf0730c4adf467e
siennalansky <siennalansky@gmail.com>
2026-04-15 17:44:22 +0200

    Params in DetailsPage
    Used find method to get the right pokemon that matches the id param

--- src/pages/DetailsPage.tsx
+++ src/pages/DetailsPage.tsx
@@ -1,12 +1,22 @@
+import { useParams } from "react-router-dom";
 import RatingStars from "../components/rating-stars/RatingStars";
+import { pokemons } from "../data/pokemon";
 
 function DetailsPage() {
+    const { id } = useParams()
+    if (!id) return <div>Invalid pokemon id</div>
+
+    const numericId = Number(id)
+    const pokemon = pokemons.find(p => p.id === numericId)
+    
+
   return (
     <div className="details-container">
-        <h2></h2>
-        <img src="" alt="" />
-        <p></p>
-        <RatingStars value={} />
+        <button>Go back</button>
+        <h2>{pokemon.name}</h2>
+        <img src={pokemon.image} alt={pokemon.name} />
+        <p>{pokemon.description}</p>
+        <RatingStars value={pokemon.rating} />
         <div className="user-rating-container">
             <button onClick={() => {}}>Rate</button>
 
