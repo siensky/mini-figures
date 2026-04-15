2eea817752edc0f00abb17d63ced3005dc7f982b
siennalansky <siennalansky@gmail.com>
2026-04-15 17:51:42 +0200

    useNavigate to route back to catalog
    Used usenavigate from react router dom to route back to catalog page from the details page

--- src/pages/DetailsPage.tsx
+++ src/pages/DetailsPage.tsx
@@ -1,26 +1,25 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import RatingStars from "../components/rating-stars/RatingStars";
 import { pokemons } from "../data/pokemon";
 
 function DetailsPage() {
-    const { id } = useParams()
-    if (!id) return <div>Invalid pokemon id</div>
+  const navigate = useNavigate();
+  const { id } = useParams();
+  if (!id) return <div>Invalid pokemon id</div>;
 
-    const numericId = Number(id)
-    const pokemon = pokemons.find(p => p.id === numericId)
-    
+  const numericId = Number(id);
+  const pokemon = pokemons.find((p) => p.id === numericId);
 
   return (
     <div className="details-container">
-        <button>Go back</button>
-        <h2>{pokemon.name}</h2>
-        <img src={pokemon.image} alt={pokemon.name} />
-        <p>{pokemon.description}</p>
-        <RatingStars value={pokemon.rating} />
-        <div className="user-rating-container">
-            <button onClick={() => {}}>Rate</button>
-
-        </div>
+      <button onClick={() => navigate("/")}>Go back</button>
+      <h2>{pokemon.name}</h2>
+      <img src={pokemon.image} alt={pokemon.name} />
+      <p>{pokemon.description}</p>
+      <RatingStars value={pokemon.rating} />
+      <div className="user-rating-container">
+        <button onClick={() => {}}>Rate</button>
+      </div>
     </div>
   );
 }
