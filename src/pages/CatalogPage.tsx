db1a6c9a959184dd1be359d4402b8572b2e41b1a
siennalansky <siennalansky@gmail.com>
2026-04-15 17:48:49 +0200

    Wrap FigureCard with Link
    Wrapped figureCard with react router dom Link to be able to route to the specifik pokemon detail page when clicked

--- src/pages/CatalogPage.tsx
+++ src/pages/CatalogPage.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import FigureCard from "../components/figure-card/FigureCard.js";
 import { pokemons } from "../data/pokemon.js";
 
@@ -7,11 +8,13 @@ function CatalogPage() {
       <div className="grid-container">
         {pokemons.map((pokemon) => (
           <div className="grid-item">
+            <Link to={`/pokemon/${pokemon.id}`} className="figure-card-link" >
             <FigureCard
               key={pokemon.id}
               pokemon={pokemon}
               rating={pokemon.rating}
             />
+            </Link>
           </div>
         ))}
       </div>
