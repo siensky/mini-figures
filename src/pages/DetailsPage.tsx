b820985c3ea2d8b40838c368b2f08a09d69f9e1e
siennalansky <siennalansky@gmail.com>
2026-04-17 22:10:25 +0200

    local storage functions

--- src/pages/DetailsPage.tsx
+++ src/pages/DetailsPage.tsx
@@ -1,14 +1,40 @@
 import { useNavigate, useParams } from "react-router-dom";
 import RatingStars from "../components/rating-stars/RatingStars";
 import { pokemons } from "../data/pokemon";
+import { useState } from "react";
 
 function DetailsPage() {
-  const navigate = useNavigate();
-  const { id } = useParams();
-  if (!id) return <div>Invalid pokemon id</div>;
+  const [isRatingOpen, setIsRatingOpen] = useState(false);
+  const [userRating, setUserRating] = useState<number>(() => getStoredRating(numericId));
 
+  const getStoredRating(id: number): number => {
+    const savedRating = localStorage.getItem(`pokemon-rating-${id}`)
+    return savedRating ? Number(savedRating) : 0 
+  }
+
+  const saveRating(id:number, rating: number) => {
+    localStorage.setItem(`pokemon-rating-${id}`, String(rating))
+  }
+
+  const navigate = useNavigate();
+
+  const { id } = useParams();
   const numericId = Number(id);
+
   const pokemon = pokemons.find((p) => p.id === numericId);
+  if (!pokemon) return <div>Pokemon not found</div>;
+
+  function handleRating(newRating: number) {
+    setUserRating(newRating);
+    saveRating(pokemon.id, newRating);
+    setIsRatingOpen(false);
+  }
+
+  const displayedRating = userRating ? userRating : pokemon.rating;
+
+  const toggleChange = () => {
+    setIsRatingOpen(!isRatingOpen);
+  };
 
   return (
     <div className="details-container">
@@ -16,10 +42,15 @@ function DetailsPage() {
       <h2>{pokemon.name}</h2>
       <img src={pokemon.image} alt={pokemon.name} />
       <p>{pokemon.description}</p>
-      <RatingStars value={pokemon.rating} />
-      <div className="user-rating-container">
-        <button onClick={() => {}}>Rate</button>
-      </div>
+      <RatingStars value={displayedRating} />
+
+      <button onClick={toggleChange}>Rate</button>
+
+      {isRatingOpen && (
+        <div className="user-rating-container">
+          <RatingStars value={0} interactive={true} onChange={handleRating} />
+        </div>
+      )}
     </div>
   );
 }
